7bb11ee7cbbe9424fd60d9fbdbcc7cf4ddfa0ed8
markakritov1993 <mark@noblescripts.com>
2021-09-06 17:05:57 +0400

    Added script to count # of read books automaticaly.

---
+++ count_books.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+
+    with open("./listbooks.md", "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    read = [x for x in lines if x.startswith("* [x]")]
+
+    cnt_read = len(read)
+
+    lines[
+        lines.index("<!-- Automated -->\n") + 2
+    ] = f": `{cnt_read}/1000`\n"
+
+    with open("./listbooks.md", "w", encoding="utf-8") as f:
+        f.writelines(lines)
